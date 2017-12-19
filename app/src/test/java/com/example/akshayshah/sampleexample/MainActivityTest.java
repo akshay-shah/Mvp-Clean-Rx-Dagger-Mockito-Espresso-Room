92bc3491c2a253d5322a00a83cc092ebe70dc9ab
akshay.shah <akshay27991@gmail.com>
2017-12-20 01:53:48 +0530

    Robolectric dependency added Unused imports removed

--- app/src/test/java/com/example/akshayshah/sampleexample/MainActivityTest.java
+++ app/src/test/java/com/example/akshayshah/sampleexample/MainActivityTest.java
@@ -1,25 +1,15 @@
 package com.example.akshayshah.sampleexample;
 
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.example.akshayshah.sampleexample.crudActivity.MainActivity;
 
-import org.apache.tools.ant.Main;
-import org.hamcrest.CustomMatcher;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.RuntimeEnvironment;
-import org.robolectric.Shadows;
-import org.robolectric.annotation.Config;
-import org.robolectric.shadows.ShadowToast;
-
-import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
 
 /**
  * Created by akshay.shah on 19/12/17.
@@ -45,5 +35,6 @@ public class MainActivityTest {
         MainActivity activity = Robolectric.buildActivity(MainActivity.class).create().get();
         Button buttonAddUser = (Button) activity.findViewById(R.id.buttonRemoveUser);
         buttonAddUser.performClick();
+        assertNotNull(buttonAddUser);
     }
 }
